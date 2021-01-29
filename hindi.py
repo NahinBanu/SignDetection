13a54b1a737b7cd43cc9015fa85b129f02ed57b8
RamizSarfaraj <ramizstudies@gmail.com>
2021-01-29 17:49:16 +0530

    source changed

---
+++ hindi.py
@@ -0,0 +1,9 @@
+from ProcessImage import Load_Image, Resize_Image, Detect_box
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import cv2
+
+print(os.getcwd())
+# os.chdir("../Signs/Originals")
+print(os.getcwd())
