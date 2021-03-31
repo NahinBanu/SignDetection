3d7bc028eaeb7379235ff62cf1b545eb9435ba38
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2021-03-31 10:14:11 +0530

    organised

--- croping.py
+++
@@ -1,5 +0,0 @@
-from ProcessImage import Load_Image, Resize_Image, Process_Image, Detect_box
-
-from_path = "/home/ramiz/signatureDetection/Signs/Originals/"
-to_path = "/home/ramiz/signatureDetection/Signs/Processed-Original"
-count = Process_Image(from_path, to_path, "Bangla", 1, first_name="BNG_")
